607bd3285fce082f153b96e6c595e270f10d4c12
Gurharpartap Singh <gurharpartapdhaliwal@gmail.com>
2021-10-10 22:29:59 +0530

    Add PostList component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,74 +2,84 @@ import React from "react";
 
 class PostList extends React.Component {
     render() { 
-        const post=this.props;
+        const {posts}=this.props;
         return (
-            <div className="post-wrapper" key={post._id}>
-            <div className="post-header">
-                 <div className="post-avatar">
-                         <img src="https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__480.jpg" alt="" />
-                         <div>
-                               <span className="post-author">
-                                 {post.user.name}
-                               </span>
+            <div className="posts-list">
+            {
+              posts.map((post)=>{
+                return (
+                  <div className="post-wrapper" key={post._id}>
+                     <div className="post-header">
+                          <div className="post-avatar">
+                                  <img src="https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__480.jpg" alt="" />
+                                  <div>
+                                        <span className="post-author">
+                                          {post.user.name}
+                                        </span>
 
-                               <span className="post-time">
-                                   few seconds ago
-                               </span>
-                       </div>
-                 </div>
+                                        <span className="post-time">
+                                            few seconds ago
+                                        </span>
+                                </div>
+                          </div>
 
 
+                         
+
+                     
+                     </div>
+
+
+
+                    <div className="post-content">
+                        {post.content}
+                    </div>
+
+                    <div className="post-actions">
+
+                          <div className="post-like">
+                          <i class="fas fa-thumbs-up"></i>                             
+                           <span>10</span>
+                          </div>
+
+
+                          <div className="post-comments-icon">
+                          <i class="far fa-comments"></i>
+                            <span>{post.comments.length}</span>
+                          </div>
+
+
+
+
+                    </div>
+
+                    <div className="post-comment-box">
+                        <input type="text" placeholder="Start typing a comment" />
+                    </div>
+
+                    <div className="post-comments-list">
+                        <div className="post-comments-item">
+                              <div className="post-comment-header">
+                                  <span className="post-comment-author">GSD</span>
+                                  <span className="post-comment-time">a minute ago</span>
+                                  <span className="post-comment-likes">100</span>
+                              </div>
+
+                              <div className="post-comment-content">
+                                    Hey nice post 
+                              </div>
+
+                        </div>
+                    </div>
+
+
+                  </div>
+                )
+              }
                 
-
-            
-            </div>
-
-
-
-           <div className="post-content">
-               {post.content}
-           </div>
-
-           <div className="post-actions">
-
-                 <div className="post-like">
-                 <i class="fas fa-thumbs-up"></i>                             
-                  <span>10</span>
-                 </div>
-
-
-                 <div className="post-comments-icon">
-                 <i class="far fa-comments"></i>
-                   <span>{post.comments.length}</span>
-                 </div>
-
-
-
-
-           </div>
-
-           <div className="post-comment-box">
-               <input type="text" placeholder="Start typing a comment" />
-           </div>
-
-           <div className="post-comments-list">
-               <div className="post-comments-item">
-                     <div className="post-comment-header">
-                         <span className="post-comment-author">GSD</span>
-                         <span className="post-comment-time">a minute ago</span>
-                         <span className="post-comment-likes">100</span>
-                     </div>
-
-                     <div className="post-comment-content">
-                           Hey nice post 
-                     </div>
-
-               </div>
-           </div>
-
-
-         </div>
+              )
+            }
+          </div>
         );
     }
 }
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import App from './App';
+import PostList from './PostList';
+
+
+export {PostList,App}
